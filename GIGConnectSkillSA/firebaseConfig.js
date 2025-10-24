209a62d94ba3346471a3cf3863a40c5a030ff66c
Mpho Matseka <ehlersdanlosboy@gmail.com>
2025-10-24 14:51:02 +0200

    Update Firebase configuration and integrate analytics

--- GIGConnectSkillSA/firebaseConfig.js
+++ GIGConnectSkillSA/firebaseConfig.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add your own Firebase configuration from your Firebase project console
 // https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBw6IotQYIYfiTXq0vvMCA9Rk8UleLNaUo",
+  authDomain: "gigconnectskill-sa.firebaseapp.com",
+  databaseURL: "https://gigconnectskill-sa-default-rtdb.firebaseio.com",
+  projectId: "gigconnectskill-sa",
+  storageBucket: "gigconnectskill-sa.firebasestorage.app",
+  messagingSenderId: "291699433541",
+  appId: "1:291699433541:web:d3b8659e61ec8ad84bc32c",
+  measurementId: "G-DCMR9LMM5B"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
